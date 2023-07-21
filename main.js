c284782decc5f2e218cb03613f6da09730b31e98
Caleb-Kao <kcaleb101@gmail.com>
2023-07-20 18:14:31 -0700

    onClick

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 console.log('hello world');
 console.log('hello world');
+
+let body = document.querySelector('body');
+// body.style.backgroundColor = 'blue';
+body.onclick = function () {
+    body.style.backgroundColor = 'blue';
+}
