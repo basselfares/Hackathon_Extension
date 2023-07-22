ee2d1af360de67d3adf29ca596eda24a3fcd343b
Caleb-Kao <kcaleb101@gmail.com>
2023-07-22 10:01:21 -0700

    cateogry page

--- main.js
+++ main.js
@@ -1,15 +1,75 @@
-// let body = document.querySelector('body');
-// let questionDiv = document.querySelector('.question');
-// body.append(questionDiv)
+let body = document.querySelector('body');
+let questionDiv = document.getElementById('question');
+let scoreDiv = document.getElementById('score');
+let streakDiv = document.getElementById('streak');
+let trueDiv = document.getElementById('trueTest');
+let falseDiv = document.getElementById('falseTest');
+let reset = document.getElementById('reset');
 
-// let roundNum = 0;
+body.append(questionDiv)
 
-// const tru = document.getElementById("trueTest")
-// const fals = document.getElementById("falseTest")
-// tru.style.border = '2px solid #FFFFFF'
-// tru.style.backgroundColor = '#eab05e'
-// fals.style.border = '2px solid #FFFFFF'
-// fals.style.backgroundColor = '#eab05e'
+let tracker = -1;
+let score = 0;
+let streak = 0;
+let tries = 0;
+let allQuestions;
+
+function getQuestions() {
+    fetch('https://opentdb.com/api.php?amount=50&type=boolean')
+        //https://opentdb.com/api.php?amount=25&category=9&difficulty=easy&type=boolean
+        .then((data) => data.json())
+        .then((data) => {
+            allQuestions = Object.values(data)[1];
+            handler();
+        })
+};
+
+function handler() {
+    tracker++;
+    questionDiv.innerHTML = allQuestions[tracker].question;
+    scoreDiv.innerHTML = `HIGH SCORE: ${score}`;
+    streakDiv.innerHTML = `W/L ratio: ${(score / tries).toFixed(2)}`;
+};
+
+function answerTrue() {
+    if (allQuestions[tracker].correct_answer === 'True') {
+        score++;
+        tries++;
+        handler();
+    }
+    else {
+        tries++;
+        handler();
+    }
+}
+
+function answerFalse() {
+    if (allQuestions[tracker].correct_answer === 'False') {
+        score++;
+        tries++;
+        handler();
+    }
+    else {
+        tries++;
+        handler();
+    }
+}
+
+function resetScore() {
+    score = 0;
+    tries = 0;
+    scoreDiv.innerHTML = `HIGH SCORE: ${score}`;
+    streakDiv.innerHTML = `W/L ratio: ${(score / tries).toFixed(2)}`;
+}
+
+
+document.addEventListener('DOMContentLoaded', getQuestions);
+trueDiv.addEventListener('click', answerTrue);
+falseDiv.addEventListener('click', answerFalse);
+reset.addEventListener('click', resetScore);
+
+
+
 
 // document.addEventListener('DOMContentLoaded', () => {
 //     fetch('https://opentdb.com/api.php?amount=50&type=boolean')
@@ -34,67 +94,6 @@
 //             // });
 //         })
 // });
-
-
-
-let body = document.querySelector('body');
-let questionDiv = document.getElementById('question');
-let scoreDiv = document.getElementById('score');
-let streakDiv = document.getElementById('streak');
-let trueDiv = document.getElementById('trueTest');
-let falseDiv = document.getElementById('falseTest');
-body.append(questionDiv)
-
-let tracker = 0;
-let score = 0;
-let streak = 0;
-let allQuestions;
-
-function getQuestions() {
-    fetch('https://opentdb.com/api.php?amount=50&type=boolean')
-        .then((data) => data.json())
-        .then((data) => {
-            allQuestions = Object.values(data)[1];
-            handler();
-        })
-};
-
-function handler() {
-    questionDiv.innerHTML = allQuestions[tracker].question;
-    scoreDiv.innerHTML = `HIGH SCORE: ${score}`;
-    streakDiv.innerHTML = `STREAK: ${streak}`;
-    tracker++;
-};
-
-function answerTrue() {
-    if (allQuestions[tracker].correct_answer === 'True') {
-        score++;
-        streak++;
-        handler();
-    }
-    else {
-        streak = 0;
-        handler();
-    }
-}
-
-function answerFalse() {
-    if (allQuestions[tracker].correct_answer === 'False') {
-        score++;
-        streak++;
-        handler();
-    }
-    else {
-        streak = 0;
-        handler();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', getQuestions);
-trueDiv.addEventListener('click', answerTrue);
-falseDiv.addEventListener('click', answerFalse);
-
-
 
 
 
---
+++ starterPage.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").addEventListener("click", myFunction);
+
+let test;
+function myFunction() {
+    window.location.href = "index.html";
+    test = 
+}
