8fd38de520726ae4b8257c9fb131f4ffcca4f519
Caleb-Kao <kcaleb101@gmail.com>
2023-07-21 22:48:56 -0700

    added stretch features

--- main.js
+++ main.js
@@ -1,8 +1,146 @@
-console.log('hello world');
-console.log('hello world');
+// let body = document.querySelector('body');
+// let questionDiv = document.querySelector('.question');
+// body.append(questionDiv)
+
+// let roundNum = 0;
+
+// const tru = document.getElementById("trueTest")
+// const fals = document.getElementById("falseTest")
+// tru.style.border = '2px solid #FFFFFF'
+// tru.style.backgroundColor = '#eab05e'
+// fals.style.border = '2px solid #FFFFFF'
+// fals.style.backgroundColor = '#eab05e'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     fetch('https://opentdb.com/api.php?amount=50&type=boolean')
+//         .then((data) => data.json())
+//         .then((data) => {
+//             // let allQuestions = [];
+//             let newDiv = document.createElement('div');
+//             let objVal = Object.values(data);
+//             questionDiv.append(newDiv);
+//             newDiv.innerHTML = objVal[1][0].question;
+//             // allQuestions.push(objVal.question);
+//             console.log(objVal[1][0]);
+//             console.log(allQuestions);
+//             // Object.values(data).forEach((element, index) => {
+//             //     if (typeof element === 'object') {
+//             //         let newDiv = document.createElement('div');
+//             //         questionDiv.append(newDiv);
+//             //         newDiv.innerHTML = element[0].question;
+//             //         allQuestions.push(element[0].question)
+//             //         console.log(element)
+//             //     }
+//             // });
+//         })
+// });
+
+
 
 let body = document.querySelector('body');
-// body.style.backgroundColor = 'blue';
-body.onclick = function () {
-    body.style.backgroundColor = 'blue';
+let questionDiv = document.getElementById('question');
+let scoreDiv = document.getElementById('score');
+let streakDiv = document.getElementById('streak');
+let trueDiv = document.getElementById('trueTest');
+let falseDiv = document.getElementById('falseTest');
+body.append(questionDiv)
+
+let tracker = 0;
+let score = 0;
+let streak = 0;
+let allQuestions;
+
+function getQuestions() {
+    fetch('https://opentdb.com/api.php?amount=50&type=boolean')
+        .then((data) => data.json())
+        .then((data) => {
+            allQuestions = Object.values(data)[1];
+            handler();
+        })
+};
+
+function handler() {
+    questionDiv.innerHTML = allQuestions[tracker].question;
+    scoreDiv.innerHTML = `HIGH SCORE: ${score}`;
+    streakDiv.innerHTML = `STREAK: ${streak}`;
+    tracker++;
+};
+
+function answerTrue() {
+    if (allQuestions[tracker].correct_answer === 'True') {
+        score++;
+        streak++;
+        handler();
+    }
+    else {
+        streak = 0;
+        handler();
+    }
 }
+
+function answerFalse() {
+    if (allQuestions[tracker].correct_answer === 'False') {
+        score++;
+        streak++;
+        handler();
+    }
+    else {
+        streak = 0;
+        handler();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', getQuestions);
+trueDiv.addEventListener('click', answerTrue);
+falseDiv.addEventListener('click', answerFalse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let riddle = ''
+// fetch('https://riddles-api.vercel.app/random')
+// .then(riddles => riddles.json())
+// .then(riddles => {
+//   let riddle = JSON.stringify(riddles);
+// })
+
+// body.style.backgroundColor = 'blue';
+// document.addEventListener("DOMContentLoaded", () => {
+//     const button = document.getElementById("change-color-button");
+//     button.onclick = function () {
+//         body.style.backgroundColor = 'blue';
+//         // let queryOptions = { active: true, lastFocusedWindow: true };
+//         // let tab = chrome.tabs.query(queryOptions);
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             // Toggle the pinned status
+//             let current = tabs[0]
+//             console.log(`Tab ID is ${current.id}`)
+//             current.style.backgroundColor = 'blue';
+//             // chrome.tabs.update(tabs[0].id, { type: "change-color" });
+//             chrome.tabs.update(current.id, { 'background-color': "red" });
+//             // document.body.style.backgroundColor = "red";
+//         })
+//     }
+// });
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const button = document.getElementById("change-color-button");
+//     button.addEventListener("click", () => {
+//         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//             chrome.tabs.sendMessage(tabs[0].id, { type: "change-color" });
+//         });
+//     });
+// });
